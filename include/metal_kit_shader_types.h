bd60e30e4d0c5ba77b49669f3ed5b7895e07cb9f
alic3 <alic3dev@gmail.com>
2025-08-08 21:31:03 -0700

    add a floor

--- include/metal_kit_shader_types.h
+++ include/metal_kit_shader_types.h
@@ -2,11 +2,41 @@
 #define __metal_kit_shader_types_h
 
 #include <simd/simd.h>
-// #include <clic3_matrix.h>
+#include <clic3_vector.h>
 
 #ifndef __METAL_VERSION__
 #define constant
 #endif
+
+
+static constant const struct clic3_vector3_float size_ground_min = {
+  .x = -200.0f,
+  .y = 0.0f,
+  .z = -200.0f
+};
+
+static constant const struct clic3_vector3_float size_ground_max = {
+  .x = 200.0f,
+  .y = 10.4345f,
+  .z = 200.0f
+};
+
+
+static constant const struct clic3_vector3_float range_ground = {
+  .x = size_ground_max.x - size_ground_min.x,
+  .y = size_ground_max.y - size_ground_min.y,
+  .z = size_ground_max.z - size_ground_min.z
+};
+
+static constant const struct clic3_vector2_unsigned_int length_vertices_ground = {
+  .x = 100,
+  .y = 100
+};
+
+static constant const struct clic3_vector2_float increment_ground = {
+  .x = range_ground.x / (float)(length_vertices_ground.x),
+  .y = range_ground.z / (float)(length_vertices_ground.y)
+};
 
 static constant const unsigned int length_objects_x = 7;
 static constant const unsigned int length_objects_y = 7;
@@ -14,7 +44,7 @@ static constant const unsigned int length_objects_z = 7;
 
 static constant const unsigned int length_objects_xyz = (
   length_objects_x * length_objects_y * length_objects_z
-);
+) + 1;
 
 typedef enum {
   metal_kit_vertex_input_index_positions = 0,
