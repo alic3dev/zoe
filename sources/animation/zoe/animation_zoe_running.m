26c3685e2db4fbc307d2c4d7940f74fb356093ea
alic3 <alic3dev@gmail.com>
2026-04-02 13:00:21 -0700

    `animation/zoe/animation_zoe_running`:add_initial

---
+++ sources/animation/zoe/animation_zoe_running.m
@@ -0,0 +1,41 @@
+#include <animation/zoe/animation_zoe_running.h>
+
+#include <metil_animation/metil_animation.h>
+#include <metil_model/metil_model.h>
+#include <metil_object/metil_object.h>
+#include <metil_rendering/metil_renderable_type.h>
+
+void zoe_animation_zoe_running_initialize(
+  struct metil_animation* metil_animation
+) {
+  metil_animation_initialize(
+    metil_animation
+  );
+
+  metil_animation->loops = (
+    metil_animation_loop_loops
+  );
+}
+
+void zoe_animation_zoe_running_start(
+  struct metil_animation* metil_animation,
+  enum metil_renderable_type metil_renderable_type,
+  void* metil_renderable
+) {
+  
+}
+
+void zoe_animation_zoe_running_poll(
+  struct metil_animation* metil_animation,
+  enum metil_renderable_type metil_renderable_type,
+  void* metil_renderable,
+  float progress
+) {
+}
+
+void zoe_animation_zoe_running_end(
+  struct metil_animation* metil_animation,
+  enum metil_renderable_type metil_renderable_type,
+  void* metil_renderable
+) {
+}  
