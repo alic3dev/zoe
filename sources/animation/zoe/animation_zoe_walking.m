4b53a954251e617d37ed110669011d92108f133b
alic3 <alic3dev@gmail.com>
2026-04-02 19:11:00 -0700

    running sneaking and walking animations

--- sources/animation/zoe/animation_zoe_walking.m
+++ sources/animation/zoe/animation_zoe_walking.m
@@ -1,6 +1,9 @@
 #include <animation/zoe/animation_zoe_walking.h>
 
 #include <model/model_zoe.h>
+#include <math_c_absolute.h>
+#include <math_c_pi.h>
+#include <math_c_sine.h>
 
 #include <metil_animation/metil_animation.h>
 #include <metil_model/metil_model.h>
@@ -36,7 +39,15 @@ void zoe_animation_zoe_walking_start(
   enum metil_renderable_type metil_renderable_type,
   void* metil_renderable
 ) {
-  
+  struct metil_model* metil_model = (
+    metil_renderable
+  );
+
+  metil_joint_propagate_reset(
+    &metil_model->joints[
+      zoe_model_joint_index_neck
+    ]
+  );  
 }
 
 void zoe_animation_zoe_walking_poll(
@@ -45,6 +56,111 @@ void zoe_animation_zoe_walking_poll(
   void* metil_renderable,
   float progress
 ) {
+  struct metil_model* metil_model = (
+    metil_renderable
+  );
+
+  float progress_sine = (
+    math_c_sine(
+      (
+        progress *
+        math_c_pi_doubled
+      ),
+      math_c_pi
+    )
+  );
+
+  if (
+    progress <= 0.5f
+  ) {
+    metil_model->joints[
+      zoe_model_joint_index_hip_left
+    ].rotation.x = (
+      -progress_sine *
+      0.5f
+    );
+
+    metil_model->joints[
+      zoe_model_joint_index_knee_left
+    ].rotation.x = (
+      progress_sine *
+      0.6f
+    );
+
+    metil_model->joints[
+      zoe_model_joint_index_hip_right
+    ].rotation.x = (
+      progress_sine *
+      0.25f
+    );
+
+    metil_model->joints[
+      zoe_model_joint_index_knee_right
+    ].rotation.x = (
+      progress_sine *
+      0.3f
+    );
+  } else {
+    metil_model->joints[
+      zoe_model_joint_index_hip_left
+    ].rotation.x = (
+      -progress_sine *
+      0.25f
+    );
+
+    metil_model->joints[
+      zoe_model_joint_index_knee_left
+    ].rotation.x = (
+      -progress_sine *
+      0.3f
+    );
+
+    metil_model->joints[
+      zoe_model_joint_index_hip_right
+    ].rotation.x = (
+      progress_sine *
+      0.5f
+    );
+
+    metil_model->joints[
+      zoe_model_joint_index_knee_right
+    ].rotation.x = (
+      -progress_sine *
+      0.6f
+    );
+  }
+
+  metil_model->joints[
+    zoe_model_joint_index_shoulder_left
+  ].rotation.x = (
+    progress_sine *
+    0.3f
+  );
+
+  metil_model->joints[
+    zoe_model_joint_index_shoulder_right
+  ].rotation.x = (
+    -progress_sine *
+    0.3f
+  );
+
+  metil_model->joints[
+    zoe_model_joint_index_elbow_left
+  ].rotation.x = (
+    math_c_absolute_float(
+      progress_sine
+    ) *
+    -0.3f
+  );
+
+  metil_model->joints[
+    zoe_model_joint_index_elbow_left
+  ].rotation.x = (
+    math_c_absolute_float(
+      progress_sine
+    ) *
+    -0.3f
+  );
 }
 
 void zoe_animation_zoe_walking_end(
