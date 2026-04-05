9a13c308f2060fe06ef302289e01dfb2cddb6003
alic3 <alic3dev@gmail.com>
2026-04-04 20:57:28 -0700

    `zoe_item`:add_additional_types

--- include/items/zoe_item.h
+++ include/items/zoe_item.h
@@ -2,7 +2,9 @@
 #define __zoe_items_zoe_item_h
 
 enum zoe_item_type {
-  zoe_item_type_consumable = 0x00
+  zoe_item_type_consumable = 0x00,
+  zoe_item_type_key        = 0x01,
+  zoe_item_type_objective  = 0x02
 };
 
 struct zoe_item {
