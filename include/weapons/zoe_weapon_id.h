a1781fc25705bb41f2ec243ab23a357848fcb1ab
alic3 <alic3dev@gmail.com>
2026-04-06 22:05:10 -0700

    `zoe_weapon_knife`:initial_definition

---
+++ include/weapons/zoe_weapon_id.h
@@ -0,0 +1,7 @@
+#ifndef __zoe_weapons_zoe_weapon_id_h
+#define __zoe_weapons_zoe_weapon_id_h
+
+#define zoe_weapon_id_knife 0x00
+
+#endif
+
---
+++ include/weapons/zoe_weapon_knife.h
@@ -0,0 +1,9 @@
+#ifndef __zoe_weapons_zoe_weapon_knife_h
+#define __zoe_weapons_zoe_weapon_knife_h
+
+#include <weapons/zoe_weapon.h>
+
+extern const struct zoe_weapon zoe_weapon_knife;
+
+#endif
+
