644d397e001009508b5bf6a618345a8899fdaa19
alic3 <alic3dev@gmail.com>
2026-04-19 21:00:05 -0700

    `data_player`:add_addtional_actions_and_attributes

--- include/data/data_player.h
+++ include/data/data_player.h
@@ -6,16 +6,25 @@
 #include <weapons/zoe_weapon.h>
 
 enum zoe_data_player_action {
-  zoe_data_player_action_none   = 0b00000000,
-  zoe_data_player_action_select = 0b00000001
+  zoe_data_player_action_none             = 0b00000000,
+  zoe_data_player_action_select           = 0b00000001,
+  zoe_data_player_action_back             = 0b00000010,
+  zoe_data_player_action_attack_primary   = 0b10000000,
+  zoe_data_player_action_attack_secondary = 0b01000000,
+  zoe_data_player_action_item_primary     = 0b00100000,
+  zoe_data_player_action_item_secondary   = 0b00010000
 };
 
 enum zoe_data_player_attributes {
-  zoe_data_player_attributes_none     = 0b00000000,
-  zoe_data_player_attributes_walking  = 0b00000001,
-  zoe_data_player_attributes_sneaking = 0b00000010,
-  zoe_data_player_attributes_running  = 0b00000100,
-  zoe_data_player_attributes_jumping  = 0b00001000
+  zoe_data_player_attributes_none                 = 0b00000000,
+  zoe_data_player_attributes_walking              = 0b00000001,
+  zoe_data_player_attributes_sneaking             = 0b00000010,
+  zoe_data_player_attributes_running              = 0b00000100,
+  zoe_data_player_attributes_jumping              = 0b00001000,
+  zoe_data_player_attributes_attacking_primary    = 0b00010000,
+  zoe_data_player_attributes_attacking_secondary  = 0b00100000,
+  zoe_data_player_attributes_item_using_primary   = 0b01000000,
+  zoe_data_player_attributes_item_using_secondary = 0b10000000
 };
 
 struct zoe_data_player {
