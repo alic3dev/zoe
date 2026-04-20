45b1a05951694da4a96f1550e598405edfeea0ea
alic3 <alic3dev@gmail.com>
2026-04-20 13:19:20 -0700

    `mesh_zoe_hair`:prettyup

--- sources/mesh/mesh_zoe_hair.c
+++ sources/mesh/mesh_zoe_hair.c
@@ -15,12 +15,21 @@ void mesh_zoe_hair_initialize(
     metil_mesh_zoe_hair
   );
 
-  unsigned char length_segments = (
+  unsigned short int length_segments_radial = (
     0xa0
   );
 
+  unsigned short int length_segment_sections = (
+    0x06
+  );
+
+  unsigned short int length_segments = (
+    length_segments_radial * 
+    length_segment_sections
+  );
+
   metil_mesh_zoe_hair->length_vertices = (
-    0x03 *
+    0x3 *
     length_segments
   );
 
@@ -48,8 +57,25 @@ void mesh_zoe_hair_initialize(
     )
   );
 
+  float height_lower = (
+    -1.0f
+  );
+
+  float height_upper = (
+    2.0f
+  );
+
+  float distance_height = (
+    height_upper -
+    height_lower
+  );
+
+  float height_top = (
+    2.5f
+  );
+
   for (
-    unsigned char index_segment = (
+    unsigned short int index_segment = (
       0x00
     );
     (
@@ -63,19 +89,49 @@ void mesh_zoe_hair_initialize(
       0x03
     );
 
+    unsigned char lower = (
+      index_segment >=
+      length_segments_radial
+    );
+
+    unsigned short int index_section = (
+      index_segment /
+      length_segments_radial
+    );
     float angle = (
       -(
         (float)
-        index_segment /
+        (
+          index_segment %
+          length_segments_radial
+        ) /
         (float)
         (
-          length_segments -
+          length_segments_radial -
           0x01
         )
-      ) +
-      math_c_pi_half
+      ) *
+      1.5f +
+      math_c_pi_half *
+      1.175f
     );
 
+    float percentage_section = (
+      (float)
+      (index_section - 0x01) /
+      (float)
+      length_segment_sections
+    );  
+
+    float percentage_section_end = (
+      (float)
+      (
+        index_section +
+        0x00
+      ) /
+      (float)
+      length_segment_sections
+    );
     float radius = (
       1.25f +
       math_c_sine(
@@ -108,7 +164,15 @@ void mesh_zoe_hair_initialize(
     metil_mesh_zoe_hair->vertices[
       offset_index_vertex
     ].y = (
-      -1.0f
+      lower 
+      ? (
+        height_upper -
+        (
+          percentage_section_end *
+          distance_height
+        )
+      )
+      : height_upper
     );
 
     metil_mesh_zoe_hair->vertices[
@@ -133,22 +197,32 @@ void mesh_zoe_hair_initialize(
       offset_index_vertex +
       0x01
     ].x = (
-      math_c_sine(
+      lower
+      ?
+math_c_sine(
         (
           angle *
           math_c_pi
         ),
         math_c_pi
-      ) *
-      radius *
-      0.5f
+      ) * radius * 0.9f      : (
+        index_segment % 2 == 0x00
+      ) ? -0.1f : 0.1f
     );
 
     metil_mesh_zoe_hair->vertices[
       offset_index_vertex +
       0x01
     ].y = (
-      2.0f
+      lower
+      ? (
+        height_upper + 0.25f -
+        (
+          percentage_section *
+          distance_height
+        )
+      )
+      : height_top
     );
 
     metil_mesh_zoe_hair->vertices[
@@ -162,8 +236,12 @@ void mesh_zoe_hair_initialize(
         ),
         math_c_pi
       ) *
-      radius *
-      0.1f
+      (
+        lower 
+        ? radius * 0.9f
+        : (
+          0.8f        )
+      )
     );
 
     angle = (
@@ -189,8 +267,9 @@ void mesh_zoe_hair_initialize(
       offset_index_vertex +
       0x02
     ].y = (
-      -1.0f
-    );
+      metil_mesh_zoe_hair->vertices[
+        offset_index_vertex
+      ].y    );
 
     metil_mesh_zoe_hair->vertices[
       offset_index_vertex +
