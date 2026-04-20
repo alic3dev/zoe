ff790076932a1c76557ef54757c7e8cc553bffec
alic3 <alic3dev@gmail.com>
2026-04-19 22:55:36 -0700

    `zoe_weapon_targeting`:add

---
+++ sources/weapons/zoe_weapon_targeting.c
@@ -0,0 +1,38 @@
+#include <weapons/zoe_weapon_targeting.h>
+
+#include <weapons/zoe_weapon.h>
+
+#include <math_c_vector.h>
+#include <math_c_vector_distance.h>
+
+float zoe_weapon_targeting_frontal_radial_half(
+  struct zoe_weapon* zoe_weapon,
+  struct math_c_vector3_float* position_player,
+  struct math_c_vector3_float* rotation_player,
+  struct math_c_vector3_float* position_enemy,
+  struct math_c_vector3_float* size_enemy
+) {
+  float distance = (
+    math_c_vector3_distance_float_fastest(
+      position_player,
+      position_enemy
+    )
+  );
+
+  if (
+    distance >
+    zoe_weapon->range
+  ) {
+    return (
+      0x00
+    );
+  }
+
+  return (
+    (
+      zoe_weapon->range -
+      distance
+    ) /
+    zoe_weapon->range
+  );
+}  
