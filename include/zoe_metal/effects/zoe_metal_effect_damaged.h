9863e2542e48f279ff8d88521565dc5b7e7f5e3e
alic3 <alic3dev@gmail.com>
2026-05-02 18:57:03 -0700

    `zoe_metal_effect_damaged`:add

---
+++ include/zoe_metal/effects/zoe_metal_effect_damaged.h
@@ -0,0 +1,10 @@
+#ifndef __zoe_zoe_metal_effects_zoe_effect_damaged_h
+#define __zoe_zoe_metal_effects_zoe_effect_damaged_h
+
+void zoe_metal_effect_damaged_apply_colours(
+  thread float4*,
+  unsigned int,
+  unsigned int
+);
+
+#endif
