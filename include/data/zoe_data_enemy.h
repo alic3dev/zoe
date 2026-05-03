64e926b847a5a89ef47af15c0df180f0ae238ce7
alic3 <alic3dev@gmail.com>
2026-05-02 18:57:54 -0700

    `zoe_data_enemy`:switch_buffer_indexing_from_metil_object_to_metal_buffers

--- include/data/zoe_data_enemy.h
+++ include/data/zoe_data_enemy.h
@@ -2,11 +2,11 @@
 #define __zoe_data_zoe_data_enemy_h
 
 #define zoe_data_enemy_buffer_index_default_object (\
-  0x02\
+  0x03\
 )
 
 #define zoe_data_enemy_buffer_index_default_model (\
-  0x04\
+  0x05\
 )
 struct zoe_data_enemy {
   unsigned short int health;
